b00d85f2e3d0f37f393255e43555c4e30ade2b89
Sergio Nuñez <snunez@agenciasur.cl>
2024-02-11 16:04:33 -0300

    remove comment

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,4 @@
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
 import './styles/app.css';
 import alienGreeting from './lib/alien-greeting.js';
 import JSConfetti from 'js-confetti';
