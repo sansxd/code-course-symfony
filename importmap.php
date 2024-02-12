350ad4239513d5afbe286da6b8b5fdda0026e800
Sergio Nuñez <snunez@agenciasur.cl>
2024-02-11 23:38:25 -0300

    just move

--- importmap.php
+++ importmap.php
@@ -18,14 +18,14 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    '@symfony/stimulus-bundle' => [
+        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
     'js-confetti' => [
         'version' => '0.12.0',
     ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
-    ],
-    '@symfony/stimulus-bundle' => [
-        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
     'tom-select' => [
         'version' => '2.3.1',
